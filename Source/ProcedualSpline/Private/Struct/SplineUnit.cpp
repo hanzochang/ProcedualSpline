d7b60268b6c098d61f445e0e48e733c5739e12c1
hanzochang <hanzo.chan@gmail.com>
2017-02-04 15:40:24 +0900

    Initialize Struct/SplineUnit

---
+++ Source/ProcedualSpline/Private/Struct/SplineUnit.cpp
@@ -0,0 +1,159 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProcedualSpline.h"
+#include "SplineUnit.h"
+
+/**
+ * Converts a ESplineUnit to String.
+ * 
+ * @param	EplineUnit
+ * @returns String
+ */
+
+FString GetSplineUnitEnumAsString(ESplineUnit EnumValue)
+{
+  const UEnum* EnumPtr = FindObject<UEnum>(ANY_PACKAGE, TEXT("ESplineUnit"), true);
+  if(!EnumPtr) return FString("Invalid");
+ 
+  return EnumPtr->GetEnumName((int32)EnumValue);
+};
+
+/**
+ * Converts a FSplineUnit to String.
+ * 
+ * @param	SplineUnit		SplineUnit members to sanitize
+ * @returns String
+ */
+
+FString FSplineUnit::ToDebugString()
+{
+	FString Result;
+	Result =
+		"Distance: " + Distance.ToString() + "  |   "  +
+		"Density: " + FString::FromInt(Density) + "  |   " + 
+		"VertexVector: " + VertexVector.ToString() + "  |   " + 
+		"Msec: " + FString::SanitizeFloat(Msec);
+
+	return Result;
+}
+
+FVector FSplineUnit::BetweenPoints()
+{
+	return Distance / Density;
+}
+
+// Unreal C++内で生成するための疑似コンストラクタ
+FSplineUnit FSplineUnit::GenerateSplineUnit(
+		ESplineUnit WaveType,
+		FVector Distance,
+		FVector StartLocation,
+		FVector VertexVector,
+		float WaveCycleCount,
+		int32 Density,
+	    float Msec
+)
+{
+	FSplineUnit SplineUnit;
+	SplineUnit.WaveType = WaveType;
+	SplineUnit.Msec = Msec;
+	SplineUnit.VertexVector = VertexVector;
+	SplineUnit.WaveCycleCount = WaveCycleCount;
+	SplineUnit.Density = Density;
+	SplineUnit.Distance = Distance;
+	SplineUnit.StartLocation = StartLocation;
+
+	return SplineUnit;
+}
+
+// SplineUnitをPointsポインタにSplineUnitの状態に応じてセットする
+void FSplineUnit::DeriveSplinePointsAddTo(TArray<FVector> &Points, FVector PrevPoint,
+	                                      FVector PrevDirection, FRotator PrevRotation)
+{
+
+	switch (WaveType)
+	{
+	case ESplineUnit::WAVE_LINEAR:
+		DeriveWaveLinearPoints(Points, PrevPoint, PrevDirection, PrevRotation);
+		break;
+	case ESplineUnit::WAVE_SIN:
+		DeriveWaveSinPoints(Points, PrevPoint, PrevDirection, PrevRotation);
+		break;
+	case ESplineUnit::WAVE_TRIANGLE:
+		DeriveWaveTrianglePoints(Points, PrevPoint);
+		break;
+	case ESplineUnit::WAVE_SAWTOOTH:
+		break;
+	}
+}
+
+void FSplineUnit::DeriveWaveLinearPoints(TArray<FVector> &Points,
+	                                     FVector PrevPoint,
+										 FVector PrevDirection,
+										 FRotator PrevRotation)
+{
+	FVector PrevDirectionVertical = FRotator{ 90, 0, 0 }.RotateVector(PrevDirection);
+	PrevDirectionVertical = FVector{ 0, 0, PrevDirectionVertical.Z };
+	FQuat quat = FQuat{ PrevDirectionVertical, FMath::DegreesToRadians(PrevRotation.Yaw) };
+
+	for (auto i = 0; i < Density; i++)
+	{
+		Points.Push(PrevPoint + BetweenPoints() * i);
+		//FVector NextPoint = quat.RotateVector(BetweenPoints());
+		//Points.Push(PrevPoint + NextPoint * i);
+	}
+}
+
+void FSplineUnit::DeriveWaveSinPoints(TArray<FVector> &Points,
+									  FVector PrevPoint,
+									  FVector PrevDirection,
+									  FRotator PrevRotation)
+{
+	for (auto i = 0; i < Density; i++)
+	{
+		float VertexBase = FMath::Sin(PI / Density * i * WaveCycleCount);
+		Points.Push(PrevPoint + BetweenPoints() * i + (VertexVector * VertexBase));
+	}
+}
+
+void FSplineUnit::DeriveWaveTrianglePoints(TArray<FVector> &Points, FVector PrevPoint)
+{
+	float Quater = Density / WaveCycleCount;
+
+	for (auto i = 0; i < Density; i++)
+	{
+		// バグあり
+		FVector BasePoint = PrevPoint + BetweenPoints() * i;
+		float NumPerQuater = i / Quater;
+		float NumPerQuaterDecimal = NumPerQuater - FMath::Floor(NumPerQuater);
+		float BranchPoint = FMath::Sin(PI * (i / Quater));
+		float BranchPointPrev = (i != 0 ? FMath::Sin(PI * ((i-1) / Quater)) : 0);
+
+		if (BranchPoint > 0)
+		{
+			if (BranchPoint - BranchPointPrev >= 0)
+			{
+				Points.Push(BasePoint + (VertexVector*2 * (NumPerQuaterDecimal)));
+			}
+			else
+			{
+				Points.Push(BasePoint + (VertexVector*2 - (VertexVector*2 * (NumPerQuaterDecimal))));
+			}
+		}
+		else if (BranchPoint < 0)
+		{
+			if (BranchPoint - BranchPointPrev >= 0)
+			{
+				Points.Push(BasePoint - (VertexVector*2 - (VertexVector*2 * (NumPerQuaterDecimal))));
+			}
+			else
+			{
+				Points.Push(BasePoint - (VertexVector*2 * (NumPerQuaterDecimal)));
+			}
+		}
+		else
+		{
+			Points.Push(BetweenPoints() * i);
+		}
+		
+	}
+}
---
+++ Source/ProcedualSpline/Public/Struct/SplineUnit.h
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "SplineUnit.generated.h"
+
+/**
+ * 
+ */
+UENUM(BlueprintType)
+enum class ESplineUnit : uint8
+{
+    WAVE_LINEAR      UMETA(DisplayName = "Linear"),
+    WAVE_SIN         UMETA(DisplayName = "Sin"),
+    WAVE_TRIANGLE    UMETA(DisplayName = "Triangle"),
+    WAVE_SAWTOOTH    UMETA(DisplayName = "Sawtooth")
+};
+
+FString GetSplineUnitEnumAsString(ESplineUnit EnumValue);
+
+USTRUCT(BlueprintType)
+struct FSplineUnit
+{
+    GENERATED_BODY()
+
+public:
+	// Distance内におけるPointsの配置方法
+    UPROPERTY(EditAnyWhere, BluePrintReadWrite, Category = "Struct")
+    ESplineUnit WaveType;
+
+	// SplineUnitから生成される最初のLocation
+    UPROPERTY(EditAnyWhere, BluePrintReadWrite, Category = "Struct")
+    FVector StartLocation;
+
+	// SplineUnitが表現する距離と方向
+	UPROPERTY(EditAnyWhere, BluePrintReadWrite, Category = "Struct")
+	FVector Distance;
+    
+	// カメラがDistance内を通過すべき時間
+    UPROPERTY(EditAnyWhere, BluePrintReadWrite, Category = "Struct")
+    float Msec;
+    
+	// WaveTypeがLinear以外の場合の曲線頂点のベクトル
+    UPROPERTY(EditAnyWhere, BluePrintReadWrite, Category = "Struct")
+    FVector VertexVector;
+
+	// WaveTypeがLinear以外の場合のWaveの折り返し数
+    UPROPERTY(EditAnyWhere, BluePrintReadWrite, Category = "Struct")
+    float WaveCycleCount;
+    
+    UPROPERTY(EditAnyWhere, BluePrintReadWrite, Category = "Struct")
+    int32 Density;
+
+public:
+	static FSplineUnit GenerateSplineUnit(
+		ESplineUnit WaveType,
+		FVector Distance,
+		FVector StartLocation,
+		FVector VertexVector,
+		float WaveCycleCount,
+		int32 Density,
+	    float Msec
+		);
+
+public:
+	FString ToDebugString();
+	FVector BetweenPoints();
+
+	void DeriveSplinePointsAddTo(TArray<FVector> &Points,
+		                         FVector PrevPoint = FVector{ 0,0,0 },
+								 FVector PrevDirection = FVector{ 0,0,0 },
+								 FRotator PrevRotation = FRotator{ 0,0,0 });
+
+private:
+	// linearカーブでのSplinePoints生成を実施
+	void DeriveWaveLinearPoints(TArray<FVector> &Points, FVector PrevPoint,
+								FVector PrevDirection, FRotator PrevRotation);
+
+	// SinカーブでのSplinePoints生成を実施
+	void DeriveWaveSinPoints(TArray<FVector> &Points, FVector PrevPoint,
+							 FVector PrevDirection, FRotator PrevRotation);
+
+	// TriangleカーブでのSplinePoints生成を実施
+	void DeriveWaveTrianglePoints(TArray<FVector> &Points, FVector PrevPoint);
+};
