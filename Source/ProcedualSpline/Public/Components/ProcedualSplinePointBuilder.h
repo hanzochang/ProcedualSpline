1c0c1908bbc0c177dd8c11b2dbd3a03db5d4bdb5
hanzochang <hanzo.chan@gmail.com>
2017-02-04 15:41:59 +0900

    Initialize ProcedualSplinePointBuilder

---
+++ Source/ProcedualSpline/Public/Components/ProcedualSplinePointBuilder.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "ProcedualSplinePointBuilder.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PROCEDUALSPLINE_API UProcedualSplinePointBuilder : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UProcedualSplinePointBuilder();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+		
+	
+};
