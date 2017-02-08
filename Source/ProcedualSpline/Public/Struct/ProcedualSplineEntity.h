c7997bb0f26e596378a42d524dcf7e00def156ac
hanzochang <hanzo.chan@gmail.com>
2017-02-08 11:19:49 +0900

    Create UStruct ProcedualSplineEntity

---
+++ Source/ProcedualSpline/Public/Struct/ProcedualSplineEntity.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ProcedualSplineEntity.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct FProcedualSplineEntity
+{
+    GENERATED_BODY()
+
+public:
+	float TotalSplineUnitLength;
+	float CurrentSplineUnitLength;
+	int32 CurrentToSplineUnitNum;
+
+	int32 PrevSplineUnitPointStartNum;
+	int32 PrevSplineUnitPointEndNum;
+
+	TArray<float> DisplayableSplineUnitLengths;
+	int32 DisplayableSplineUnitLimit;
+	float DisplayableSplineUnitLength;
+};
