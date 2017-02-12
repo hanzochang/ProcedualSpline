61cf0c818aaad4a9b69edf7a6e19e64d58abfb0e
hanzochang <hanzo.chan@gmail.com>
2017-02-13 08:31:47 +0900

    Created ProcedualSplineActorsBuilder

---
+++ Source/ProcedualSpline/Public/Components/ProcedualSplineActorsBuilder.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/PrimitiveComponent.h"
+#include "ProcedualSplineActorsBuilder.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCEDUALSPLINE_API UProcedualSplineActorsBuilder : public UPrimitiveComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
